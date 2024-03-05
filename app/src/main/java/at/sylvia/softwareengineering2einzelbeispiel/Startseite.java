fc01f1dafe036fb813698727c32012e9ad2d4e7e
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-03-05 17:38:19 +0100

    Initial commit

---
+++ app/src/main/java/at/sylvia/softwareengineering2einzelbeispiel/Startseite.java
@@ -0,0 +1,14 @@
+package at.sylvia.softwareengineering2einzelbeispiel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Startseite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startseite);
+    }
+}
