c35f4b38ee6a12e2dc10289d077aa4651bfa767f
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-03-05 18:43:28 +0100

    Initializing Button,Eingabefeld and Serverantwort

--- app/src/main/java/at/sylvia/softwareengineering2einzelbeispiel/Startseite.java
+++ app/src/main/java/at/sylvia/softwareengineering2einzelbeispiel/Startseite.java
@@ -3,12 +3,22 @@ package at.sylvia.softwareengineering2einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Startseite extends AppCompatActivity {
-
+    //Initialisierungen von Button, Eingabefeld und Serverantwort
+    private EditText eingabeMatNR;
+    private Button sendButton;
+    private TextView serverAntwort;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startseite);
+
+        eingabeMatNR = findViewById(R.id.eingabeMatNR);
+        sendButton = findViewById(R.id.sendButton);
+        //serverAntwort = findViewById(R.id.);
     }
 }
