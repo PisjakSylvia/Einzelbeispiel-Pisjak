ab0904b302502b0d4023891de9e6f43a48326be7
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-03-05 19:40:17 +0100

    Created getMatBerechnung() to set up server

--- app/src/main/java/at/sylvia/softwareengineering2einzelbeispiel/Startseite.java
+++ app/src/main/java/at/sylvia/softwareengineering2einzelbeispiel/Startseite.java
@@ -3,15 +3,28 @@ package at.sylvia.softwareengineering2einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Startseite extends AppCompatActivity {
     //Initialisierungen von Button, Eingabefeld und Serverantwort
-    private EditText eingabeMatNR;
+    private static EditText eingabeMatNR;
     private Button sendButton;
     private TextView serverAntwort;
+    private static final String SERVER_Domain = "se2-submission.aau.at";
+    private static final int SERVER_Port = 20080;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,54 @@ public class Startseite extends AppCompatActivity {
 
         eingabeMatNR = findViewById(R.id.eingabeMatNR);
         sendButton = findViewById(R.id.sendButton);
-        //serverAntwort = findViewById(R.id.);
+        serverAntwort = findViewById(R.id.serverAntwort);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMatBerechnung();
+            }
+        });
+    }
+
+    private void getMatBerechnung() {
+        final String matrikelnummer = eingabeMatNR.getText().toString();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket(SERVER_Domain, SERVER_Port);
+                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    out.write(matrikelnummer);
+                    out.newLine();
+                    out.flush();
+
+                    final String response = in.readLine();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverAntwort.setText(response);
+                        }
+                    });
+
+                    out.close();
+                    in.close();
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(Startseite.this, "Fehler beim Senden der Daten", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+        thread.start();
     }
 }
